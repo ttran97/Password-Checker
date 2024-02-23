30a43338695155ae0e20e6c4da4a8fcd04ce3a85
Tony Tran <64768038+ttran97@users.noreply.github.com>
2024-02-23 13:22:01 -0500

    Add files via upload

---
+++ Password Checker/script.js
@@ -0,0 +1,82 @@
+// script.js 
+  
+let password = document.getElementById("password"); 
+let power = document.getElementById("power-point"); 
+password.oninput = function () { 
+    let point = 0; 
+    let value = password.value; 
+    let widthPower =  
+        ["1%", "25%", "50%", "75%", "100%"]; 
+    let colorPower =  
+        ["#D73F40", "#DC6551", "#F2B84F", "#BDE952", "#3ba62f"]; 
+  
+    if (value.length >= 6) { 
+        let arrayTest =  
+            [/[0-9]/, /[a-z]/, /[A-Z]/, /[^0-9a-zA-Z]/]; 
+        arrayTest.forEach((item) => { 
+            if (item.test(value)) { 
+                point += 1; 
+            } 
+        }); 
+    } 
+    power.style.width = widthPower[point]; 
+    power.style.backgroundColor = colorPower[point]; 
+};
+
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
